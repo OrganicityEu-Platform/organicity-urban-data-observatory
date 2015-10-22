ae055892ca256cc3a263e55b481d54563bbcb12f
Pral2a <guillem@pral2a.com>
2015-10-23 01:32:41 +0300

    removed legacy readings

--- src/app/core/api/sensor.service.js
+++ src/app/core/api/sensor.service.js
@@ -15,7 +15,6 @@
         callAPI: callAPI,
         setTypes: setTypes,
         getTypes: getTypes,
-        getSensorsData: getSensorsData,
         getSensorsDataNew: getSensorsDataNew
       };
       return service;
@@ -32,19 +31,6 @@
 
       function getTypes() {
         return sensorTypes;
-      }
-
-      function getSensorsData(deviceID, dateFrom, dateTo) {
-        var rollup = sensorUtils.getRollup(dateFrom, dateTo);
-
-        dateFrom = utils.convertTime(dateFrom);
-        dateTo = utils.convertTime(dateTo);
-        if(!dateFrom || !dateTo) {
-          return Restangular.one('devices', deviceID).customGET('pg_readings');        
-        }
-
-        /*jshint camelcase: false */
-        return Restangular.one('devices', deviceID).customGET('pg_readings', {'from': dateFrom, 'to': dateTo, all_intervals: true, rollup: rollup});
       }
 
       function getSensorsDataNew(deviceID, sensorID, dateFrom, dateTo) {
