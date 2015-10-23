40279ff9a288e66d24578b0d1eaf0b4097914e7c
Pral2a <guillem@pral2a.com>
2015-10-23 14:31:42 +0300

    Deploy repo URL

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.0'
 
 
 set :application, 'oc-observatory-web'
-set :repo_url, 'git@github.com:fablabbcn/smartcitizen-web.git'
+set :repo_url, 'git@github.com:pral2a/smartcitizen-web.git'
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
