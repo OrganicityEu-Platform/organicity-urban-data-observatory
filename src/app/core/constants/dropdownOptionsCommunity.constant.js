777d7eef8c7e80d66ba27c2d119ee0ec80cb501b
Pral2a <guillem@pral2a.com>
2015-10-24 05:13:14 +0300

    Updated community menu

--- src/app/core/constants/dropdownOptionsCommunity.constant.js
+++ src/app/core/constants/dropdownOptionsCommunity.constant.js
@@ -9,9 +9,8 @@
 
   angular.module('app.components')
     .constant('DROPDOWN_OPTIONS_COMMUNITY', [
-      {text: 'Forum', href: 'https://forum.smartcitizen.me/'},
-      {text: 'Documentation', href: 'http://docs.smartcitizen.me/#/'},
-      {text: 'API Reference', href: 'http://api.smartcitizen.me/'},
-      {text: 'Github', href: 'https://github.com/fablabbcn/Smart-Citizen-Kit'}        
+      {text: 'About', href: 'http://organicity.eu'},
+      {text: 'Events', href: 'http://organicity.eu/events'},
+      {text: 'Github', href: 'https://github.com/organicityeu'}        
     ]);
 })();
