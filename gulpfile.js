f41adbc378b1fb8d847be0592b82948c8e32da4a
Viktor Smari <viktorsmari@gmail.com>
2017-10-11 16:03:03 +0200

    Write version in footer

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,22 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
   require('./gulp/' + file)(options);
 });
 
+gulp.task('version', function(){
+  var p = require('./package.json');
+  var replace = require('replace');
+
+  gutil.log(' -- The version is: ' + p.version);
+
+  replace({
+    regex: "Version.*",
+    replacement: "Version: " + p.version,
+    paths: ['./src/app/components/footer/footer.html'],
+    recursive: true,
+    silent: true,
+  });
+
+});
+
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
