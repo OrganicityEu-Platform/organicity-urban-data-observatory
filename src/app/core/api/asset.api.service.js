21021a6df439a8f5c9b429557e59d2f7af409e23
Viktor Smari <viktorsmari@gmail.com>
2017-10-20 13:52:15 +0200

    Temp dns change

--- src/app/core/api/asset.api.service.js
+++ src/app/core/api/asset.api.service.js
@@ -5,7 +5,7 @@
   angular.module('app.components')
     .factory('assetsAPI', function(Restangular) {
       return Restangular.withConfig(function(RestangularConfigurer) {
-          RestangularConfigurer.setBaseUrl('https://discovery.organicity.eu/v0'); //http://discovery.organicity.eu/v0
+          RestangularConfigurer.setBaseUrl('https://dev.discovery.organicity.eu/v0'); //http://discovery.organicity.eu/v0
           //RestangularConfigurer.setBaseUrl('http://localhost/v0');
         });
     });
